5997e26b827e371409fc218803328d81c08ced40
Raghd <raghadzeidan@mail.tau.ac.il>
2020-05-01 23:48:59 +0300

    added graph dependency

--- app/src/main/java/com/example/gostudy/StatsFragment.java
+++ app/src/main/java/com/example/gostudy/StatsFragment.java
@@ -4,16 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-public class StatsFragment extends Fragment {
-
+public class StatsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment_stats, container, false);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
