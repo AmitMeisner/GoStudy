9da76ff4d3e299d630e61191207d1f01a626a8a1
Ori <orijoseph@mail.tau.ac.il>
2020-05-04 06:57:41 +0300

    new feature: google sign in. make sure to download google play service from tools -> sdk manager -> sdk tools.

--- app/src/main/java/com/example/gostudy/TipsFragment.java
+++ app/src/main/java/com/example/gostudy/TipsFragment.java
@@ -1,21 +1,39 @@
 package com.example.gostudy;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
-import java.util.zip.Inflater;
 
 public class TipsFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_tips, container, false);
+        final View root = inflater.inflate(R.layout.fragment_tips, container, false);
+
+        Button submitTipBtn = (Button) root.findViewById(R.id.submitTipBtn);
+
+        submitTipBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                EditText user_tip=(EditText) root.findViewById(R.id.user_tip);
+                TextView res=(TextView) root.findViewById(R.id.result);
+                String text= user_tip.getEditableText().toString();
+                res.setText(text);
+                user_tip.setText("");
+            }
+        });
+        return root;
+
     }
 
 }
